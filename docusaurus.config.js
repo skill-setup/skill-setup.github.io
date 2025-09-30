3318910058277265eb1e0a6d3b965143b800238d
Franz Stimpfl <franz.stimpfl@agilecoderz.com>
2025-09-30 21:11:53 +0200

    setup github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,16 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://skill-setup.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'skill-setup', // Usually your GitHub org/user name.
+  projectName: 'skill-setup.github.io', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
